d55a5675e10c029dec3207fb034fd2e0ca3bd193
mason tsai <39562999+masontsaii@users.noreply.github.com>
2022-12-01 16:19:42 -0800

    Update vecadd_gpu_256t_manyblocks_prefetch.cu

--- vecadd_gpu_256t_manyblocks_prefetch.cu
+++ vecadd_gpu_256t_manyblocks_prefetch.cu
@@ -1,57 +1,54 @@
 #include <iostream>
 #include <math.h>
 // Kernel function to add the elements of two arrays
-
-__global__
-void add(int n, float *x, float *y)
+global
+void add(int n, float x, floaty)
 {
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-    int stride = blockDim.x * gridDim.x;
-    for (int i = index; i < n; i += stride)
-        y[i] = x[i] + y[i];
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i += stride)
+    y[i] = x[i] + y[i];
 }
 
 int main(void)
 {
-    int N = 1<<24;
-    float *x, *y;
+  int N = 1<<24;
+  float x,y;
 
-    // Allocate Unified Memory – accessible from CPU or GPU
-    cudaMallocManaged(&x, N*sizeof(float));
-    cudaMallocManaged(&y, N*sizeof(float));
+  // Allocate Unified Memory – accessible from CPU or GPU
+  cudaMallocManaged(&x, Nsizeof(float));
+  cudaMallocManaged(&y, Nsizeof(float));
+ 
 
-    int deviceID = 0;
-    cudaMemPrefetchAsync((void *)x, N*sizeof(float), deviceID);
-    cudaMemPrefetchAsync((void *)y, N*sizeof(float), deviceID);
+  // initialize x and y arrays on the host
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
 
-    // initialize x and y arrays on the host
-    for (int i = 0; i < N; i++) {
-        x[i] = 1.0f;
-        y[i] = 2.0f;
-    }
+  int deviceID=0;
+  cudaMemPrefetchAsync((void )x, Nsizeof(float), deviceID);
+  cudaMemPrefetchAsync((void )y, Nsizeof(float), deviceID);
 
-    // Run kernel on 1M elements on the GPU
-    add<<<1, 256>>>(N, x, y);
+  int blockSize = 256;
+  int numBlocks = (N + blockSize - 1) / blockSize;
+  add<<<numBlocks, blockSize>>>(N, x, y);
 
+  //Prints number of thread blocks
+  std::cout << "Number of thread blocks: " << numBlocks << std::endl;
 
-    // Wait for GPU to finish before accessing on host
-    cudaDeviceSynchronize();
+  // Wait for GPU to finish before accessing on host
+  cudaDeviceSynchronize();
 
-    int blockSize = 256;
-    int numBlocks = (N + blockSize - 1) / blockSize;
-    add<<<numBlocks, blockSize>>>(N, x, y);
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
 
-    std::cout << "Number of thread blocks " << numBlocks << std::endl;
+  // Free memory
+  cudaFree(x);
+  cudaFree(y);
 
-    // Check for errors (all values should be 3.0f)
-    float maxError = 0.0f;
-    for (int i = 0; i < N; i++)
-        maxError = fmax(maxError, fabs(y[i]-3.0f));
-    std::cout << "Max error: " << maxError << std::endl;
-
-    // Free memory
-    cudaFree(x);
-    cudaFree(y);
-
-    return 0;
+  return 0;
 }
